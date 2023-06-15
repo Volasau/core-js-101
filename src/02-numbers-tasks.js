9dfb969126be5f1e3e56b067cef9bf7c64d57d39
Ryhor Volasau <G.volosov@gmail.com>
2023-06-15 23:44:43 +0400

    feat: add solution number six

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -107,8 +107,12 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const skalyr = x1 * x2 + y1 * y2;
+  const lengthOne = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const lengthTwo = Math.sqrt(x2 ** 2 + y2 ** 2);
+  const restul = Math.acos(skalyr / (lengthOne * lengthTwo));
+  return restul;
 }
 
 /**
