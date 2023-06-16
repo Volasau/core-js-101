1e16c4705bfe741da2b3e8875aa0287a40a12eb1
Ryhor Volasau <G.volosov@gmail.com>
2023-06-16 19:21:22 +0400

    feat: add solutions arrays-task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -246,8 +246,13 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  let sum = 0;
+  const result = arr.map((el) => {
+    sum += el;
+    return sum;
+  });
+  return result;
 }
 
 /**
@@ -261,8 +266,9 @@ function getMovingSum(/* arr */) {
  * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
  * [ "a" ] => []
  */
-function getSecondItems(/* arr */) {
-  throw new Error('Not implemented');
+function getSecondItems(arr) {
+  const result = arr.filter((_, ind) => ind % 2 !== 0);
+  return result;
 }
 
 /**
